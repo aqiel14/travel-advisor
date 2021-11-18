5f70bec598c4ef242bd7a03ebb398208182fc6d7
aqiel14 <72314836+aqiel14@users.noreply.github.com>
2021-11-19 02:26:55 +0700

    search bar Google places API

--- src/App.js
+++ src/App.js
@@ -12,33 +12,62 @@ const App = () => {
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
 
+  const [childClicked, setChildClicked] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       ({ coords: { latitude, longitude } }) => {
-        console.log(latitude);
         setCoordinates({ lat: latitude, lng: longitude });
       }
     );
   }, []);
 
   useEffect(() => {
-    getPlacesData().then((data) => {
-      setPlaces(data);
-    });
-  }, [coordinates, bounds]);
+    const filteredPlaces = places.filter((place) => place.rating > rating);
+
+    setFilteredPlaces(filteredPlaces);
+  }, [rating]);
+
+  useEffect(() => {
+    if (bounds.sw && bounds.ne) {
+      setIsLoading(true);
+      getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      });
+    }
+  }, [type, bounds]);
   return (
     <>
       <CssBaseline />
-      <Header />
+      <Header setCoordinates={setCoordinates} />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List
+            places={filteredPlaces.length ? filteredPlaces : places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
